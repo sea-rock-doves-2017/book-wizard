fb837b968d805301816eeee39cf296147f910b8d
Kris <email": null>
2017-07-19 16:51:03 -0700

    add one test for post #create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,41 +1,35 @@
 require 'rails_helper'
 
 describe UsersController do
+
   let!(:user) { User.create!(username: 'Bob', email: 'b@b.com', password: 'blah')}
 
   describe "GET #new" do
-
     it "responds with 200" do
       get :new
       expect(response.status).to eq 200
     end
-
     it "assigns a new user to @user" do
       get :new
       expect(assigns(:user)).to be_a_new User
     end
-
     it "renders the :new template" do
       get :new
       expect(response).to render_template(:new)
     end
-
   end
 
   xdescribe "POST #create" do
-
     it "responds with 302" do
+      post :create, { user2: { username: 'jane', email: 'jane@j.com', password: 'poop'}}
+      expect(response.status).to eq 302
     end
-
-    it "creates new user in DB" do
+    xit "creates new user in DB" do
     end
-
-    it "redirects to root" do
+    xit "redirects to root" do
     end
-
-    it "renders the :new template for invalid user params" do
+    xit "renders the :new template for invalid user params" do
     end
-
   end
 
 end
