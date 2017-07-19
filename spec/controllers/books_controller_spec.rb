6914c607e648f8a5b583319e16b0c5bba761ddbe
Kris <email": null>
2017-07-19 16:29:05 -0700

    add tests for books#index

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe BooksController, type: :controller do
+describe BooksController do
+  let!(:genre1) { Genre.create!(name: 'Fiction')}
+  let!(:book1) { Book.create!(title: "B", author: "G", synopsis: "X", genre_id: genre1.id)}
+
+  describe "GET #index" do
+
+    it "responds with status code 200" do
+      get :index
+      expect(response.status).to eq 200
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns books as @books" do
+      get :index
+      books = Book.all
+      expect(assigns(:books)).to eq books
+    end
+
+  end
+
+  describe "GET #show" do
+  end
 
 end
