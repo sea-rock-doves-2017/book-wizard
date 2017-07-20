8f9f8d5f564f61b84ac9d3084342b34dc7d4949c
Kris <email": null>
2017-07-19 17:25:42 -0700

    add review_params method and routes for creating a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,21 @@
 class ReviewsController < ApplicationController
+  include ReviewsHelper
 
   def index
+    @reviews = Review.all
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      render 'show'
+    else
+      render 'new'
+    end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-  
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,6 @@
 module ReviewsHelper
+  # Not sure if I need to permit user_id and book_id
+  def review_params
+   params.require(:review).permit(:body)
+  end
 end
